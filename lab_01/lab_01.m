c1e9106c1ac806bfa59b3882ff2754e34c5279bf
Nikolai2038 <nikolai2038@yandex.ru>
2024-09-14 18:49:07 +0300

    Lab 01: Done

--- lab_01/lab_01.m
+++ lab_01/lab_01.m
@@ -3,7 +3,7 @@ classdef lab_01 < matlab.apps.AppBase
 
     % Properties that correspond to app components
     properties (Access = public)
-        UIFigure                        matlab.ui.Figure
+        ModelSignalGeneratorUIFigure    matlab.ui.Figure
         NumberOfAccumulationsEditField  matlab.ui.control.NumericEditField
         NumberOfAccumulationsLabel      matlab.ui.control.Label
         ModelSignalGeneratorLabel       matlab.ui.control.Label
@@ -100,130 +100,130 @@ classdef lab_01 < matlab.apps.AppBase
         % Create UIFigure and components
         function createComponents(app)
 
-            % Create UIFigure and hide until all components are created
-            app.UIFigure = uifigure('Visible', 'off');
-            app.UIFigure.Position = [100 100 631 496];
-            app.UIFigure.Name = 'MATLAB App';
+            % Create ModelSignalGeneratorUIFigure and hide until all components are created
+            app.ModelSignalGeneratorUIFigure = uifigure('Visible', 'off');
+            app.ModelSignalGeneratorUIFigure.Position = [100 100 631 496];
+            app.ModelSignalGeneratorUIFigure.Name = 'Model Signal Generator';
 
             % Create UIAxes
-            app.UIAxes = uiaxes(app.UIFigure);
+            app.UIAxes = uiaxes(app.ModelSignalGeneratorUIFigure);
             xlabel(app.UIAxes, 'X')
             ylabel(app.UIAxes, 'Y')
             zlabel(app.UIAxes, 'Z')
             app.UIAxes.Position = [201 77 410 360];
 
             % Create StartButton
-            app.StartButton = uibutton(app.UIFigure, 'push');
+            app.StartButton = uibutton(app.ModelSignalGeneratorUIFigure, 'push');
             app.StartButton.ButtonPushedFcn = createCallbackFcn(app, @StartButtonPushed, true);
             app.StartButton.BackgroundColor = [0.9137 1 0.8];
             app.StartButton.Position = [299 30 100 23];
             app.StartButton.Text = 'Start';
 
             % Create QuitButton
-            app.QuitButton = uibutton(app.UIFigure, 'push');
+            app.QuitButton = uibutton(app.ModelSignalGeneratorUIFigure, 'push');
             app.QuitButton.ButtonPushedFcn = createCallbackFcn(app, @QuitButtonPushed, true);
             app.QuitButton.BackgroundColor = [1 0.8588 0.8588];
             app.QuitButton.Position = [446 30 100 23];
             app.QuitButton.Text = 'Quit';
 
             % Create PeriodsNumberLabel
-            app.PeriodsNumberLabel = uilabel(app.UIFigure);
+            app.PeriodsNumberLabel = uilabel(app.ModelSignalGeneratorUIFigure);
             app.PeriodsNumberLabel.HorizontalAlignment = 'right';
             app.PeriodsNumberLabel.Position = [56 134 95 22];
             app.PeriodsNumberLabel.Text = 'Periods Number:';
 
             % Create PeriodsNumberEditField
-            app.PeriodsNumberEditField = uieditfield(app.UIFigure, 'numeric');
+            app.PeriodsNumberEditField = uieditfield(app.ModelSignalGeneratorUIFigure, 'numeric');
             app.PeriodsNumberEditField.ValueDisplayFormat = '%9.1f';
             app.PeriodsNumberEditField.HorizontalAlignment = 'center';
             app.PeriodsNumberEditField.Position = [28 113 155 22];
             app.PeriodsNumberEditField.Value = 2;
 
             % Create NoiseSKOLabel
-            app.NoiseSKOLabel = uilabel(app.UIFigure);
+            app.NoiseSKOLabel = uilabel(app.ModelSignalGeneratorUIFigure);
             app.NoiseSKOLabel.HorizontalAlignment = 'right';
             app.NoiseSKOLabel.Position = [68 197 68 22];
             app.NoiseSKOLabel.Text = 'Noise SKO:';
 
             % Create NoiseSKOEditField
-            app.NoiseSKOEditField = uieditfield(app.UIFigure, 'numeric');
+            app.NoiseSKOEditField = uieditfield(app.ModelSignalGeneratorUIFigure, 'numeric');
             app.NoiseSKOEditField.ValueDisplayFormat = '%9.1f';
             app.NoiseSKOEditField.HorizontalAlignment = 'center';
             app.NoiseSKOEditField.Position = [28 176 155 22];
             app.NoiseSKOEditField.Value = 0.1;
 
             % Create SignalAmplitudeLabel
-            app.SignalAmplitudeLabel = uilabel(app.UIFigure);
+            app.SignalAmplitudeLabel = uilabel(app.ModelSignalGeneratorUIFigure);
             app.SignalAmplitudeLabel.HorizontalAlignment = 'right';
             app.SignalAmplitudeLabel.Position = [54 312 98 22];
             app.SignalAmplitudeLabel.Text = 'Signal Amplitude:';
 
             % Create SignalAmplitudeEditField
-            app.SignalAmplitudeEditField = uieditfield(app.UIFigure, 'numeric');
+            app.SignalAmplitudeEditField = uieditfield(app.ModelSignalGeneratorUIFigure, 'numeric');
             app.SignalAmplitudeEditField.ValueDisplayFormat = '%9.1f';
             app.SignalAmplitudeEditField.HorizontalAlignment = 'center';
             app.SignalAmplitudeEditField.Position = [28 291 155 22];
             app.SignalAmplitudeEditField.Value = 1;
 
             % Create NoiseTypeLabel
-            app.NoiseTypeLabel = uilabel(app.UIFigure);
+            app.NoiseTypeLabel = uilabel(app.ModelSignalGeneratorUIFigure);
             app.NoiseTypeLabel.HorizontalAlignment = 'right';
             app.NoiseTypeLabel.Position = [70 255 68 22];
             app.NoiseTypeLabel.Text = 'Noise Type:';
 
             % Create NoiseTypeDropDown
-            app.NoiseTypeDropDown = uidropdown(app.UIFigure);
+            app.NoiseTypeDropDown = uidropdown(app.ModelSignalGeneratorUIFigure);
             app.NoiseTypeDropDown.Items = {'Normally Distributed', 'White Gaussian Noise'};
             app.NoiseTypeDropDown.Position = [28 234 155 22];
             app.NoiseTypeDropDown.Value = 'Normally Distributed';
 
             % Create NumberOfPointsLabel
-            app.NumberOfPointsLabel = uilabel(app.UIFigure);
+            app.NumberOfPointsLabel = uilabel(app.ModelSignalGeneratorUIFigure);
             app.NumberOfPointsLabel.HorizontalAlignment = 'right';
             app.NumberOfPointsLabel.Position = [51 362 104 22];
             app.NumberOfPointsLabel.Text = 'Number Of Points:';
 
             % Create NumberOfPointsEditField
-            app.NumberOfPointsEditField = uieditfield(app.UIFigure, 'numeric');
+            app.NumberOfPointsEditField = uieditfield(app.ModelSignalGeneratorUIFigure, 'numeric');
             app.NumberOfPointsEditField.ValueDisplayFormat = '%.0f';
             app.NumberOfPointsEditField.HorizontalAlignment = 'center';
             app.NumberOfPointsEditField.Position = [28 341 155 22];
             app.NumberOfPointsEditField.Value = 1024;
 
             % Create SignalTypeLabel
-            app.SignalTypeLabel = uilabel(app.UIFigure);
+            app.SignalTypeLabel = uilabel(app.ModelSignalGeneratorUIFigure);
             app.SignalTypeLabel.HorizontalAlignment = 'right';
             app.SignalTypeLabel.Position = [68 419 71 22];
             app.SignalTypeLabel.Text = 'Signal Type:';
 
             % Create SignalTypeDropDown
-            app.SignalTypeDropDown = uidropdown(app.UIFigure);
+            app.SignalTypeDropDown = uidropdown(app.ModelSignalGeneratorUIFigure);
             app.SignalTypeDropDown.Items = {'Harmonic (Sinusoidal)', 'Sawtooth', 'Triangular', 'Rectangular Pulses'};
             app.SignalTypeDropDown.Position = [28 398 155 22];
             app.SignalTypeDropDown.Value = 'Harmonic (Sinusoidal)';
 
             % Create ModelSignalGeneratorLabel
-            app.ModelSignalGeneratorLabel = uilabel(app.UIFigure);
+            app.ModelSignalGeneratorLabel = uilabel(app.ModelSignalGeneratorUIFigure);
             app.ModelSignalGeneratorLabel.HorizontalAlignment = 'center';
             app.ModelSignalGeneratorLabel.FontSize = 24;
             app.ModelSignalGeneratorLabel.Position = [2 451 630 31];
             app.ModelSignalGeneratorLabel.Text = 'Model Signal Generator';
 
             % Create NumberOfAccumulationsLabel
-            app.NumberOfAccumulationsLabel = uilabel(app.UIFigure);
+            app.NumberOfAccumulationsLabel = uilabel(app.ModelSignalGeneratorUIFigure);
             app.NumberOfAccumulationsLabel.HorizontalAlignment = 'right';
             app.NumberOfAccumulationsLabel.Position = [32 51 148 22];
             app.NumberOfAccumulationsLabel.Text = 'Number Of Accumulations:';
 
             % Create NumberOfAccumulationsEditField
-            app.NumberOfAccumulationsEditField = uieditfield(app.UIFigure, 'numeric');
+            app.NumberOfAccumulationsEditField = uieditfield(app.ModelSignalGeneratorUIFigure, 'numeric');
             app.NumberOfAccumulationsEditField.ValueDisplayFormat = '%.0f';
             app.NumberOfAccumulationsEditField.HorizontalAlignment = 'center';
             app.NumberOfAccumulationsEditField.Position = [29 30 155 22];
             app.NumberOfAccumulationsEditField.Value = 1;
 
             % Show the figure after all components are created
-            app.UIFigure.Visible = 'on';
+            app.ModelSignalGeneratorUIFigure.Visible = 'on';
         end
     end
 
@@ -237,7 +237,7 @@ classdef lab_01 < matlab.apps.AppBase
             createComponents(app)
 
             % Register the app with App Designer
-            registerApp(app, app.UIFigure)
+            registerApp(app, app.ModelSignalGeneratorUIFigure)
 
             % Execute the startup function
             runStartupFcn(app, @startupFcn)
@@ -251,7 +251,7 @@ classdef lab_01 < matlab.apps.AppBase
         function delete(app)
 
             % Delete UIFigure when app is deleted
-            delete(app.UIFigure)
+            delete(app.ModelSignalGeneratorUIFigure)
         end
     end
 end
